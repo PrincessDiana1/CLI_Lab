7f1855cdd596336973e02e03f10edc6c192f6a77
Diana Eboh <dianaeboh@dianas-air.home>
2023-02-07 16:47:32 +0000

    final

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.example.Flights;
+import org.example.Passengers;
+
+public class Test {
+
+@Override
+        System.out.println("Enter passenger name");
+    String name = sc.nextLine();
+        System.out.println("Enter contact information");
+
+
+    }
+
